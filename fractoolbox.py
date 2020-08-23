5d3ba156a51fd0af4ae5d271d423bf1bf78a86e2
Irene Wallis <irene@cubicearth.nz>
2020-08-23 13:26:53 +1200

    Simplify function and add comments

--- fractoolbox.py
+++ fractoolbox.py
@@ -1,3 +1,4 @@
+############################################################################# 80
 '''
 ============================
 fractoolbox Function Library
@@ -11,16 +12,58 @@ fractoolbox Function Library
 # ============================
 '''
 
+
+-------------------
+The Right-Hand Rule
+-------------------
+Place your right hand on the fracture plane with fingers pointed down
+the direction of dip. Your thumb points to the strike azumuth which is
+used under the right-hand rule convention. 
 '''
 
-def dip2strike(dipazim,dip):
-    '''Convert dip-dipazimuth data to strike 
-    using the right-hand rule convention'''
-    if dipazim < 90:
-        strike=(dipazim-90)+360
+def dip2strike(dipaz):
+    '''Convert dip-dipazimuth data to strike using the right-hand rule
+    #
+    -----
+    Input
+    -----
+    dipaz
+        Azumuth of dip in degrees from north
+    #
+    ------
+    Output
+    ------
+    return
+        Strike azumuth in degrees from north 
+        which adhears to the right hand rule
+    '''
+    if dipaz < 90:
+        strike=(dipaz-90)+360
     else:
-        strike=dipazim-90
+        strike=dipaz-90
     return strike
+
+def strike2dipaz(strike):
+    '''Convert strike to dip azumuth using the right-hand rule convention
+    #
+    -----
+    Input
+    -----
+    strike
+        Strike azumuth in degrees from north 
+        where the right-hand rule is observed
+    #
+    ------
+    Output
+    ------
+    return
+        Azumuth of the dip direction (0-360 degrees)
+    '''
+    if strike > 270:
+        dipaz=(strike+90)-360
+    else:
+        dipaz=strike+90
+    return dipaz
 
 # Geometric Sample Bias: Isogenic Contours
 # ========================================
