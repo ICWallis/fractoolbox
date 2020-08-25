29c6541d0d3c1b8c03f7c4dd5a35819b32558820
Irene Wallis <irene@cubicearth.nz>
2020-08-25 20:44:38 +1200

    Comment and point to correct function

--- demo-presentation/thermal-stress.py
+++ demo-presentation/thermal-stress.py
@@ -5,16 +5,21 @@ import functions as fun
 # ============================================
 # Model of thermal stress at the borehole wall
 # ============================================
-# Kerish equations from Jayger and Cook
-# with some implemtation method from Zoback
-
+'''
+Kirsh (1898) equations are used to calcuate the stresses resolved onto
+the wall of a borehole where the axis is parallel to the vertical component
+of the stress tensor. 
+Equations as presented in Jager et al. (2007) and Zoback (2010).
+'''
 
 # Thermal Stress Magnatude
 # ------------------------
 '''
 Calculate the magnatude of thermally induced stess [MPa] for a  
 range of reservior tempratures and assuming steady state conditions.
-The convention of - as tensile and + as compressive is used here
+The convention of - as tensile and + as compressive is used here.
+The thermal stress is subtracted from the hoop stress curves 
+and generate a shift in the negitive direction. 
 '''
 Twell = 40 + 273.14 # drilling gluid temp in Kelvin
 therex = 1.e-5 # coefficent of thermal expansion
@@ -56,11 +61,13 @@ Sv = 88.232
 Shmin = 20 
 Pp = 13.
 Pmud = 14.75
+deltaP = Pmud - Pp
 
 # fixed paramaters 
 nu = 0.1
-R = 1
-r = 1.0
+R = 1 # wellbore radius
+r = 1.0 # depth of investigation
+# where R = r we are at the borehole wall
 n = 200
 
 
@@ -69,15 +76,22 @@ plt.figure('Sigma rr',figsize=(10,5))
 # angles around the borehole wall
 theta = fun.ftheta(n)
 
-
+# Radial stress
 #rr = stress.fsigma_rr(SHmax, Shmin, Pp, Pmud, R, r, theta)
 #plt.plot(theta*180/np.pi,rr,label=r'$\sigma_{rr}$')
 
+# Stress along the axis of the borehole
 #zz = stress.fsigma_zz(SHmax,Sv,Shmin,nu,Pp,R,r,theta,sigma_Dt) # the mean of this should = the mean stress?
 #plt.plot(theta*180/np.pi,zz,label=r'$\sigma_{zz}$')
 
-tt = stress.fsigma_tt(SHmax, Shmin, Pp, Pmud, 0., R, r, theta)
+tt = fun.effhoopstress(SHmax, Shmin, Pp, Pmud, 0., R, r, theta)
 plt.plot(theta*180/np.pi,tt,label=r'$\sigma_{\theta \theta}$ with DT = 0')
+
+#tt2 = fun.fsigma_tt(SHmax, Shmin, deltaP)
+#plt.plot(theta*180/np.pi,tt2,label=r'$\fsigma_{\theta \theta}$ with DT = 0')
+
+plt.show()
+asdf
 
 tt = stress.fsigma_tt(SHmax, Shmin, Pp, Pmud, -2., R, r, theta)
 plt.plot(theta*180/np.pi,tt,label=r'$\sigma_{\theta \theta}$ with DT = 10')
