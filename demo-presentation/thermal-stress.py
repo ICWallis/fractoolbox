ac9fc1200583d1fed22909cc43047aa3e614205d
Irene Wallis <irene@cubicearth.nz>
2020-08-25 22:22:08 +1200

    Trim excess from code

--- demo-presentation/thermal-stress.py
+++ demo-presentation/thermal-stress.py
@@ -1,16 +1,16 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import functions as fun
-
 # ============================================
 # Model of thermal stress at the borehole wall
 # ============================================
 '''
-Kirsh (1898) equations are used to calculate the stresses resolved onto
-the wall of a borehole where the axis is parallel to the vertical component
-of the stress tensor. 
+Kirsh (1898) equations are used to calculate the stresses resolved onto the wall of 
+a borehole. In this case, the borehole is vertical (i.e., the borehole axis is 
+parallel to the vertical component of the stress tensor so the plots reflect the 
+various influence of the two horizontal stresses as we move around the borehole wall. 
 Equations as presented in Jager et al. (2007) and Zoback (2010).
 '''
+import numpy as np
+import matplotlib.pyplot as plt
+import functions as fun
 
 # Thermal Stress Magnatude
 # ------------------------
@@ -35,16 +35,8 @@ for n in rtemps:
     x = fun.thermal_stress(therex, K, nu, Tres, Twell)
     thermal_stress_lst.append(x)
 
-print(thermal_stress_lst)
-
-#sigma_Dt = np.asarray(sigma_Dt_lst)
-#print(sigma_Dt)
-
-# Geomechanical model paramaters
-# ------------------------------
-'''
-Stress state and fluid pressures for a single depth 
-'''
+# Model paramaters
+# ----------------
 
 # strike slip faulting case
 # stress/pressure in MPa
@@ -65,18 +57,22 @@ Pmud = 14.75
 # fixed paramaters
 R = 1 # wellbore radius
 r = 1.0 # depth of investigation
-# where R = r we are at the borehole wall
+# R = r at the borehole wall
 
 # angles around the borehole wall
 n = 200
 theta = fun.theta(n)
 
+# Calculate and plot
+# ------------------
 f, (ax1,ax2) = plt.subplots(1, 2, figsize = (15, 5), sharey=True)
 
+# hoop stress - normal faulting case
 for thermstress, rtemp in zip(thermal_stress_lst,rtemps):
     tt = fun.effhoopstress(SHmax_nf, Shmin_nf, Pp, Pmud, thermstress, R, r, theta)
     ax1.plot(theta*180/np.pi,tt,label=rtemp)
 
+# hoop stress - strike slip case
 for thermstress, rtemp in zip(thermal_stress_lst,rtemps):
     tt = fun.effhoopstress(SHmax_ss, Shmin_ss, Pp, Pmud, thermstress, R, r, theta)
     ax2.plot(theta*180/np.pi,tt,label=rtemp)
@@ -90,17 +86,16 @@ for ax in [ax1,ax2]:
           alpha=0.2)
 
 # compressive rock strength
+UCS = [(20,30),(70,100)]
+for ax in [ax1,ax2]:
+    for top, bottom in UCS:
+        ax.axhspan(top, bottom, color='k', alpha=.2)
 '''
 From Wyering et al (2014)
 -   shallower samples with low-temperature alteration UCS = 27.7 ± 10.3 MPa
 -   deeper samples with high-temperature alteration 84.8 ± 30.6 MPa
 Range used here is 20-30 for shallow and 70-100 for deep
 '''
-
-UCS = [(20,30),(70,100)]
-for ax in [ax1,ax2]:
-    for top, bottom in UCS:
-        ax.axhspan(top, bottom, color='k', alpha=.2)
 
 for ax in [ax1,ax2]:
     ax.set_xlabel(r'Angle around the borehole wall [deg]')
@@ -116,9 +111,3 @@ ax2.set_title('Strike-slip faluting (large difference in horizontal stress)' )
 
 plt.suptitle('Same depth (Sv) with a fixed ' + str(Twell_degC) + 'degC mud and varying reservior temp')
 plt.show()
-
-
-
-
-
-
