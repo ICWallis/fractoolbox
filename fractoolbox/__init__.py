c97aa95d0781993a1a7c3ac73d1ca9c7b6d91b80
Irene Wallis <irene@cubicearth.nz>
2020-09-04 07:41:20 +1200

    Debug init and test on notebooks

--- fractoolbox/__init__.py
+++ fractoolbox/__init__.py
@@ -1,10 +1,10 @@
-from .data-wrangling import dip2strike
-from .data-wrangling import strike2dipaz
-from .data-wrangling import xyzinterp
-from .geometric-bias import unitvectorx
-from .geometric-bias import unitvectory
-from .geometric-bias import unitvectorz
-from .geometric-bias import isogeniccontour
+from .data_wrangling import dip2strike
+from .data_wrangling import strike2dipaz
+from .data_wrangling import xyzinterp
+from .geometric_bias import unitvectorx
+from .geometric_bias import unitvectory
+from .geometric_bias import unitvectorz
+from .geometric_bias import isogeniccontour
 
 
 __version__ = '0.1-dev'
