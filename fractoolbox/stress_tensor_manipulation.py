f4fb3185c40e5d978acebf9d131cff7c3aab9a62
Irene Wallis <irene@cubicearth.nz>
2025-03-13 10:29:14 +1300

    Combine stress tensor functions into one file

--- fractoolbox/stress_tensor_manipulation.py
+++ fractoolbox/stress_tensor_manipulation.py
@@ -1,29 +1,46 @@
-# Series of functions coded from Peska and Zoback (1995), with corrections from Zoback 2010,
-# to model the maximum horizontal stress magnitude and orientation on an inclined borehole. 
+# ====================================================================
+# Stress Tensor Manipulation for Hoop Stress and Fracture Calculations
+# ====================================================================
+'''
+Kirsh (1898) Equations for Stress Resolved onto a Circular Cavity
+
+The Kirsh equations describe effective stresses around a wellbore in 
+cylindrical coordinates for a vertical well and are used when the 
+borehole axis = Sv. These can be used to explore the impact horizontal stress,
+pore pressure and thermal stress have on the formation of drilling induced
+borehole damage (i.e., borehole breakout and drilling induced tensile fractures).
+
+Kirsch, 1898, Die theorie der elastizität und die bedürfnisse der festigkeitslehre: 
+Zeitschrift des Vereines deutscher Ingenieure, v. 42, p. 797–807.
+
+The functions below are from: 
+
+    Jaeger, J. C., 2007, Fundamentals of rock mechanics, Malden, MA, Malden, 
+    MA : Blackwell Pub. 2007.
+
+    Zoback, M. D., 2010, Reservoir Geomechanics, Cambridge University Press.
+
+Contributions
+-------------
+fractoolbox was initiated by Irene Wallis https://github.com/ICWallis/fractoolbox
+as part of Doctoral Research at the University of Auckland that is 
+supervised by David Dempsey https://github.com/ddempsey and 
+Julie (JR) Rowland, with math/code contributions from Evert Durán 
+https://github.com/edur409.
+
+Licence 
+-------
+fractoolbox is distributed under an Apache 2.0 license
+https://choosealicense.com/licenses/apache-2.0/
+'''
 
 import numpy as np
 from matplotlib import pyplot as plt
 import math
 
-def normalise_by_value(input_list, value):
-    result = []
-    for n in input_list:
-        result.append(n/value) 
-    return result
-
-def radians_to_degrees(radians):
-    '''
-    Convert list of radians to list of degrees
-
-        Args: radians (list or 1D array) angle in radians
-
-        Returns: list of given angles in degrees
-    '''
-    result = []
-    for r in radians:
-        degrees = math.degrees(r)
-        result.append(degrees)
-    return result
+#
+# Make Stress Tensor
+#
 
 
 def make_stress_tensor(S1,S2,S3):
@@ -77,6 +94,91 @@ def make_effective_stress_tensor(S1,S2,S3,Pp):
     return result
 
 
+#
+# Transform Stress Tensor
+#
+
+
+def geographic_rotation_array(alpha,beta,gamma):
+    '''Returns array used to transform an initial stress tensor into the geographic coordinate system 
+    
+        Args:   alpha (float) Euler angle alpha in degrees (refer to notes below)
+                beta (float) Euler angle alpha in degrees (refer to notes below)
+                gamma (float) Euler angle alpha in degrees (refer to notes below)
+
+        Returns:    Array used for transformation
+                    Referred to as Rs by Peska/Zoback 
+
+        Notes:  Function called by transform_from_initial_to_geographic (fSg) which does the 
+                matrix multiplication to execute the transformation
+
+                Geographic coordinates are X North, Y East, and Z Down.
+                
+                Defining the stress field in Euler angles:
+                    
+                    If S1 is vertical (normal faulting) then:
+                        alpha = the trend of SHmax - pi/2 (aka the azimuth in degrees minus 90 degrees)
+                        beta = the -ve trend of Sv (aka -90 for vertical stress)
+                        gamma = 0.
+                    
+                    If S1 is horizontal (strike slip or reverse faulting) then:
+                        alpha = trend of S1
+                        beta = -ve plunge of S1
+                        gamma = rake of S2
+                
+                NOTE Function used to be called fRs
+
+        Citation:   Peska and Zoback (1995)
+    '''
+    alpha = math.radians(alpha)
+    beta = math.radians(beta)
+    gamma = math.radians(gamma)
+
+    Rs = np.array([
+        [np.cos(alpha) * np.cos(beta), 
+         np.sin(alpha) * np.cos(beta), 
+         -np.sin(beta) ],
+        [np.cos(alpha) * np.sin(beta) * np.sin(gamma) - np.sin(alpha) * np.cos(gamma), 
+         np.sin(alpha) * np.sin(beta) * np.sin(gamma) + np.cos(alpha) * np.cos(gamma), 
+         np.cos(beta) * np.sin(gamma) ],
+        [np.cos(alpha) * np.sin(beta) * np.cos(gamma) + np.sin(alpha) * np.sin(gamma),
+         np.sin(alpha) * np.sin(beta) * np.cos(gamma) - np.cos(alpha) * np.sin(gamma),
+         np.cos(beta) * np.cos(gamma) ]
+    ])
+    return Rs 
+
+
+def fracture_rotation_array(strike,dip):
+    '''Generate array used to transform the stress tensor from geographic to fracture/fault plane coordinates
+
+        Args:   strike (float)  Fracture strike in degrees
+                dip (float)     Fracture dip in degrees
+
+        Returns:    2D array used to do transformation
+                    Referred to as Rf by Zoback (2010)
+
+        Notes:  Strike and dip must follow the right hand rule
+
+                If strike and dip doesn't follow the right hand rule and 
+                the fault dipped to the left when viewed along strike, 
+                then the dip would be a negative number (not ideal for handling here)
+
+                Function is called by the fSf that does the transformation 
+
+                NOTE function used to be called fRf
+        
+        Citation: Zoback (2010) pp 156-157
+    '''
+    strike = math.radians(strike)
+    dip = math.radians(dip)
+
+    Rf = np.array([
+        [np.cos(strike), np.sin(strike), 0],
+        [np.sin(strike)*np.cos(dip), -np.cos(strike)*np.cos(dip), -np.sin(dip)],
+        [-np.sin(strike)*np.sin(dip), np.cos(strike)*np.sin(dip), -np.cos(dip)]
+    ])
+
+    return Rf
 
 
 def borehole_rotation_array(well_azimuth,well_inclination):
@@ -104,15 +206,9 @@ def borehole_rotation_array(well_azimuth,well_inclination):
     phi = math.radians(well_inclination)
 
     Rb = np.array([
-        [-np.cos(delta) * np.cos(phi), 
-        -np.sin(delta) * np.cos(phi), 
-        np.sin(phi)],
-        [np.sin(delta), 
-        -np.cos(delta),
-        0],
-        [np.cos(delta) * np.sin(phi),
-        np.sin(delta) * np.sin(phi),
-        np.cos(phi)]
+        [-np.cos(delta) * np.cos(phi), -np.sin(delta) * np.cos(phi), np.sin(phi)],
+        [np.sin(delta), -np.cos(delta), 0],
+        [np.cos(delta) * np.sin(phi), np.sin(delta) * np.sin(phi), np.cos(phi)]
     ])
 
     return Rb
@@ -169,6 +265,32 @@ def transform_from_initial_to_borehole(initial_stress_tensor, alpha, beta, gamma
     return result
 
 
+
+#
+# Hoop Stress
+#
+# Series of functions coded from Peska and Zoback (1995), with corrections from Zoback 2010,
+# to model the maximum horizontal stress magnitude and orientation on an inclined borehole. 
+
+
+def theta(n):
+    """Generates a list of radian values between 0 and 2 pi (i.e., 0 to 360 degrees)
+    
+    Used in many of the functions calculating properties around the borehole wall.
+    By convention, theta is measured from the SHmax azimuth.
+    
+    Args:
+        n (float): The number of numbers that will be generated at equal spacing 
+        between 0 and 360 degrees including the start and finish value.
+    
+    Returns:
+        (float) A list of numbers in radians (typically referred to as theta)
+
+    """
+    theta = np.linspace(0, 2 * np.pi, n)
+    return theta
+
+
 def calculate_sigma_zz(theta, sigma_one, sigma_two, sigma_three, sigma_tauA, nu):
     '''
     Calculate 'vertical' stress on borehole (along the borehole axis)
@@ -201,6 +323,7 @@ def calculate_sigma_zz(theta, sigma_one, sigma_two, sigma_three, sigma_tauA, nu)
             )
         result.append(s_zz)
     return result
+
 
 def calculate_sigma_tt(theta, sigma_one, sigma_two, sigma_tauA, deltaP):
     '''
@@ -235,6 +358,7 @@ def calculate_sigma_tt(theta, sigma_one, sigma_two, sigma_tauA, deltaP):
         result.append(s_tt)
     return result
 
+
 def calculate_tau_tz(theta, sigma_tauB, sigma_tauC):
     '''
     Calculate shear stress on borehole
@@ -261,6 +385,7 @@ def calculate_tau_tz(theta, sigma_tauB, sigma_tauC):
         t_tz =   (2 * (sigma_tauB * np.cos(t) - sigma_tauC * np.sin(t)))
         result.append(t_tz)
     return result
+
 
 def calculate_sigma_rr(theta, deltaP):
     '''
@@ -336,6 +461,7 @@ def calculate_sigma_tmax(sigma_zz, sigma_tt, tau_tz):
         result.append(s_tmax)
     return result 
 
+
 def calculate_sigma_tmin(sigma_zz, sigma_tt, tau_tz):
     '''
     Calculate minimum effective stress on plane tangential to the borehole wall (sigma_tmin)
@@ -356,6 +482,105 @@ def calculate_sigma_tmin(sigma_zz, sigma_tt, tau_tz):
         result.append(s_tmin)
     return result 
 
+
+def thermal_stress(therex, K, nu, Tres, Twell):
+    """Thermally induced stress [MPa] assuming a steady state has been reached
+    
+    In fractoolbox, tensile stress is -ve and compressive stress is +ve
+    This convention means that Twell-Tres is used to find the deltaT 
+    and that thermal stress is added to the hoop stress calculations 
+    (eg the function calculate_effective_hoop_stress). This is the opposite convection to 
+    to what is used in Zoback (2010) pp 174 eq 6.4.
+
+    Args:
+        therex (float): Coefficient of thermal expansion, 
+            which is typically 1.e-5 per Kelvin
+        K (float): Bulk modulus, which is typically 1.e10
+        nu (float): Poisson's ratio, which is typically 0.25
+            Ensure that the elastic moduli (K & nu) are internally consistent
+        Tres (float): Reservoir temperature in Kelvin
+        Twell (float): Internal well temperature in Kelvin, 
+            which is typically ~40degC for a high-temperature geothermal well 
+            that was logged by a borehole image tool under injection 
+            but this can be higher if well permeability is low.
+
+    Returns:
+        float: Thermally induced stress (sigma_Dt)
+    
+    Function written by Irene using eq 7.150  P204 Jager et al (2007)
+    """
+    sigma_Dt = (
+        (
+            3 * therex * K 
+            * ((1 - 2 * nu) / (1 - nu))
+            * (Twell - Tres)
+        )
+        / 1.e6)
+    return sigma_Dt
+
+
+def calculate_effective_hoop_stress(SHmax, Shmin, Pp, Pmud, sigma_Dt, R, r, theta):
+    """Calculates the magnitude of the effective hoop stress around a vertical borehole
+    
+    As a convention, effective hoop stress is referred to as $\sigma_{\theta\theta}$.
+    Tension here conceptualized as a -ve value (note how deltaT is calculated in this function), 
+    so we add sigma_Dt rather than subtracting as the equation appears in Zoback after Kirsh.
+    Note that when R = r, we are at the borehole wall.
+    
+    Args:
+        SHmax (float): Magnitude of the maximum horizontal stress MPa (total stress not effective stress)
+        Shmin (float): Magnitude of the minimum horizontal stress MPa (total stress not effective stress)
+        Pp (float): Pore pressure in MPa
+        Pmud (float): Pressure inside the well in MPa (consider equivalent circulating density)
+        sigma_Dt (float): Magnitude of thermal stress MPa (refer to the fsigma_Dt function where deltaT = Twell - Tres)
+        R (float): Wellbore radius
+        r (float): Depth of investigation as radial distance from the centre of the well
+        theta (float): the azimuths around the wellbore signam_rr will be calculated for (refer to the ftheta function) 
+    
+    Returns:
+        (float) A list effective hoop stress at azimuths specified by theta (ie $sigma_{\tau\tau}$)
+
+    Function written by Evert using Kirsh (1898) as presented in Jager et al. (2007) and Zoback (2010)
+    
+    """
+    sigma_tt = (
+                0.5 * (SHmax + Shmin - 2 * Pp)
+                * (1 + (R/r)**2)
+                - 0.5 * (SHmax - Shmin)
+                * (1 + 3 * (R/r)**4)
+                * np.cos(2 * theta)
+                - (Pmud - Pp)
+                * (R/r)**2
+                + sigma_Dt
+                )
+    return sigma_tt
+
+
+#
+# Hoop stress plots
+#
+
+
+def normalise_by_value(input_list, value):
+    result = []
+    for n in input_list:
+        result.append(n/value) 
+    return result
+
+
+def radians_to_degrees(radians):
+    '''
+    Convert list of radians to list of degrees
+
+        Args: radians (list or 1D array) angle in radians
+
+        Returns: list of given angles in degrees
+    '''
+    result = []
+    for r in radians:
+        degrees = math.degrees(r)
+        result.append(degrees)
+    return result
 
 
 def peska_plot(
@@ -494,8 +719,8 @@ def theta_omega_printer(
     Function that pints the theta (distance from low side)
     and omega (tilt) of the tensile fracture
 
-    THis function has the assumptions that make S1 always vertical
-
+    This function assumes that S1 always vertical
+    S2 cases are varied to generate SS/RF conditions
     '''
     # make empty lists to append thetas and omegas to
     thetas = []
@@ -594,3 +819,149 @@ def theta_omega_printer(
         # which is also the angle of the tensile fracture relative to the borehole axis (one of our knows)
         # omega + 90 is the minimum principle stress on the tangential plane, and the value often used by peska
     return thetas, omegas
+
+
+#
+# Fracture Stress Calculations
+#
+
+
+def find_fracture_rake(Sf):
+    '''Boolean expression used to calculate the rake of a fracture
+    
+    Input the stress tensor in the coordinate system of the fracture.
+    Output is the rake of the fracture. 
+    
+    Output is used in transform_tensor_from_fracture_plane_to_rake (Rt) to generate an array that 
+    transformations (rotates) the stress tensor into the the rake vector.
+    
+    Function is called by fSr where the transformation into the rake vector occurs
+
+    Contains optional print statements to show which statement is true
+    Method from pp 156-157 in Zoback (2010)
+    '''
+    A = Sf[2,1]
+    B = Sf[2,0]
+    if A > 0.00001 and B > 0.00001:
+        rake = np.arctan(A/B) 
+        #print('r is case 1')
+    elif A > 0.00001 and B < 0.00001:
+        rake = np.arctan(A/B) 
+        #print('r is case 2')
+    elif A < 0.00001 and B >= 0.00001:
+        rake = math.radians(180)-np.arctan(A/-B)
+        #print('r is case 3')
+    elif A < 0.00001 and B < 0.00001:
+        rake = np.arctan(-A/-B)-math.radians(180)
+        #print('r is case 4')
+    return rake
+
+
+def transform_tensor_from_fracture_plane_to_rake(rake):
+    '''
+    Generates an array for that's used to transform the stress tensor from fracture plane coordinates into the rake vector
+    
+    Input is the rake of the fracture/fault generated by the function find_fracture_rake.
+    Output is called by fSr used to transformation (rotate) the stress tensor into the rake vector.
+
+    where |Sr(3,1)| is the shear stress magnitude (tau)
+
+    Method from pp 156-157 in Zoback (2010)
+    '''
+    Rt = np.array([
+                [np.cos(rake),np.sin(rake),0],
+                [-np.sin(rake),np.cos(rake),0],
+                [0,0,1]
+                ])
+    return Rt
+
+
+# Template for the use of the funcations above
+
+def fracture_sn_tau(S1,S2,S3,Pp,Norm,alpha,beta,gamma,strike,dip):
+    '''Calculate the shear (tau) and normal (Sn) stress on a fracture
+
+    Normalised to either vertical stress or effective vertical stress
+
+    Args:
+        S1:
+        S2:
+        S3:
+        Pp:
+        Sv:
+        alpha:
+        beta:
+        gamma:
+        strike:
+        dip:
+
+        Recommendation: this can be efficiently done using a tuple 
+    
+    Returns: 
+        Sn: Stress normal to the fracture plane [MPa]
+        tau: Shear stress on the fracture plane [MPa]
+
+    '''
+    '''
+    # create effective stress array
+    Ss = np.array([                 
+                    [S1-Pp,0,0],
+                    [0,S2-Pp,0],
+                    [0,0,S3-Pp]
+                    ])
+    #print('Ss: the effective stress tensor =','\n',Ss,'\n')
+
+    # use the three Euiler angles to generate an array that is used 
+    # to transform the effective stress array into geographic coordinates
+    # x has been added to Rs to differentiate it to Rs above
+    Rsx = Rs(alpha,beta,gamma)  
+    #print('Rs: the stress coordinate system based on' + 
+    #   'the inputted Euler angles =','\n',Rsx,'\n')
+
+    # rotate the stress tensor into geographic coordinates
+    Sg = Rsx.T@Ss@Rsx                 
+    #print('Sg: the effective stress tensor now rotated into' +
+    #   'geographic coordinates =','\n',Sg,'\n')
+
+    # use the fracture strike an dip to generate an array that is used
+    # to transform the stress field into fracture coordinates
+    Rfx = Rf(strike,dip)        
+    #print('Rf: the matrix that rotates the stress tensor from' + '
+    #   'geographic coordinates into the fracture plane coordinates =','\n',Rf,'\n')
+
+    # transform the stress field into the fracture coordinate system
+    # x has been added to Rf to differentiate it to Rf above
+    Sf = Rfx@Sg@Rfx.T                 
+    #print('Sf: the effective stress tensor now rotated into the' +'
+    #   fracture plane coordinate system =','\n',Sf,'\n')
+
+    # take stress normal to the fault plane and normalise it to 
+    # vertical stress so fractures from different depths can be plotted together
+    Sn = Sf[2,2]/Norm                 
+    #print('Sn: the effective stress magnitude normal to the' + '
+    #   'fault plane (Sf bottom right) normalised to Sv =','\n',Sn,'\n')
+
+    # calculate the rake of the fracture assuming only dip-slip
+    # x has been added to rake to differentiate it from rake function above
+    rakex = rake(Sf)            
+    #print('the rake of the slip vector =',rake,'\n')
+
+    # use the rake to generate an array to transform the stress field into the rake
+    Rtx = Rt(rakex)
+    #print('Array Rt that is used to transform the effective stress from the' + 
+    #   'fault co-ordinate system into the rake coordinate system so we can' + 
+    #   'grab the shear stress magnitude along the slip vector =','\n',Rt,'\n')
+
+    # transform the stress field into the direction of the rake
+    # x has been added to Rt to differentiate it from the function above
+    Sr = Rtx@Sf@Rtx.T
+    #print('Effective stress tensor along the slip vector (Sr) where the' + 
+    #   'bottom left (as an absolute number) is the shear stress magnitude (tau) =','\n',Sr,'\n')
+
+    # take the absolue number of shear stress in the direction of the rake 
+    # and normalise to vertical stress for plotting
+    tau = abs(Sr[2,0])/Norm
+    #print('Shear stress on the plane (tau, which is bottom left of Sr array) =',tau,'\n')
+
+    return (Sn,tau)
+    '''
