7a444a43be8ad23edb265c25985d004a984b4d0c
Irene Wallis <irene@cubicearth.nz>
2025-03-13 11:18:03 +1300

    Remove function groups that were refactored out

--- fractoolbox/__init__.py
+++ fractoolbox/__init__.py
@@ -50,36 +50,6 @@ from .stress_tensor_manipulation import (
 
 )
 
-from .hoops_stress_inclined_borehole import (
-    normalise_by_value,
-    radians_to_degrees,
-    make_stress_tensor,
-    make_effective_stress_tensor,
-    borehole_rotation_array,
-    transform_from_initial_to_borehole,
-    calculate_sigma_zz,
-    calculate_sigma_tt,
-    calculate_tau_tz,
-    calculate_sigma_rr,
-    calculate_omega_angle,
-    calculate_sigma_tmax,
-    calculate_sigma_tmin,
-    peska_plot,
-    theta_omega_printer,
-)
-
-from .transform_stress_tensor import (
-    #Rs,
-    geographic_rotation_array,
-    Rf,
-    rake,
-    Rt,
-    fracture_sn_tau,
-    fracture_rotation_array,
-    find_fracture_rake,
-    transform_tensor_from_fracture_plane_to_rake,
-)
-
 from .stress_models import (
     simple_linear_Sv,
     Sv,
